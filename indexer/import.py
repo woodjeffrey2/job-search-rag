240104918da5789f8a31b803166435baf4f222b6
Jeff Wood <woodjeffrey2@gmail.com>
2024-03-01 09:54:42 -0800

    ruff format

--- indexer/import.py
+++ indexer/import.py
@@ -5,14 +5,15 @@ import weaviate
 import yaml
 import uuid
 
-INPUT_FILE = './indexer/data/experiences.yml'
-CLASS_NAME = 'Experience'
+INPUT_FILE = "./indexer/data/experiences.yml"
+CLASS_NAME = "Experience"
 
 client = weaviate.connect_to_local()
 client.timeout_config = (3, 200)
 
+
 def add_experiences(data):
-    """ upload professional experience to Weaviate
+    """upload professional experience to Weaviate
 
     :param data: experiences data in panda dataframe object
     :type data: panda dataframe object (2 columns: 'description', 'job')
@@ -20,23 +21,25 @@ def add_experiences(data):
     with client.batch.dynamic() as batch:
         for index, row in data.iterrows():
             exp = row.to_dict()
-            print('Adding experience:', exp)
-            batch.add_object(properties=row.to_dict(), collection=CLASS_NAME, uuid=uuid.uuid4())
+            print("Adding experience:", exp)
+            batch.add_object(
+                properties=row.to_dict(), collection=CLASS_NAME, uuid=uuid.uuid4()
+            )
 
-    message = str(index+1) + ' / ' + str(data.shape[0]) +  ' items imported'
+    message = str(index + 1) + " / " + str(data.shape[0]) + " items imported"
     print(message)
 
 
-with open(INPUT_FILE, 'r') as file:
+with open(INPUT_FILE, "r") as file:
     yaml_data = yaml.safe_load(file)
-experiences_data = yaml_data.get('experiences', [])
+experiences_data = yaml_data.get("experiences", [])
 df = pd.DataFrame(experiences_data)
 
 try:
-  add_experiences(df, batch_size=99, debug_mode=True)
+    add_experiences(df, batch_size=99, debug_mode=True)
 
 except Exception as e:
-  print('Error: ' + e)
+    print("Error: " + e)
 
 finally:
-  client.close()
+    client.close()
--- indexer/schema.py
+++ indexer/schema.py
@@ -1,18 +1,20 @@
-
 import weaviate
 import weaviate.classes as wvc
 
-CLASS_NAME = 'Experience'
+CLASS_NAME = "Experience"
+
 
 def create_collection_schema(client):
-    """ Create schema for professional experience in Weaviate
+    """Create schema for professional experience in Weaviate
 
     :param client: Weaviate client to use to create the collection schema
     :type client: WeaviateClient (V4)
     """
     client.collections.create(
         name=CLASS_NAME,
-        vectorizer_config=wvc.config.Configure.Vectorizer.text2vec_openai(vectorize_collection_name=False),
+        vectorizer_config=wvc.config.Configure.Vectorizer.text2vec_openai(
+            vectorize_collection_name=False
+        ),
         generative_config=wvc.config.Configure.Generative.openai(),
         properties=[
             wvc.config.Property(
@@ -20,24 +22,25 @@ def create_collection_schema(client):
                 description="description of the professional experience",
                 data_type=wvc.config.DataType.TEXT,
                 vectorize_property_name=False,
-                tokenization=wvc.config.Tokenization.WORD
+                tokenization=wvc.config.Tokenization.WORD,
             ),
             wvc.config.Property(
                 name="job",
                 data_type=wvc.config.DataType.TEXT,
                 vectorize_property_name=False,
-                tokenization=wvc.config.Tokenization.WORD
+                tokenization=wvc.config.Tokenization.WORD,
             ),
-        ]
+        ],
     )
+
 
 client = weaviate.connect_to_local()
 try:
-  create_collection_schema(client)
-  print('Schema created successfully')
+    create_collection_schema(client)
+    print("Schema created successfully")
 
 except Exception as e:
-  print('Error: ' + e)
+    print("Error: " + e)
 
 finally:
-  client.close()
+    client.close()
